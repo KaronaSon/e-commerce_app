afc1c8a9e7e47960ae570ba912f1e94fa6d3f8a8
karona son <karonason@karonas-MacBook-Pro.local>
2025-02-05 10:37:30 +0700

    delete about

--- lib/pages/settings/about_us.dart
+++ lib/pages/settings/about_us.dart
@@ -16,25 +16,7 @@ class _AboutUsState extends State<AboutUs> {
       ),
       body: Column(
         children: [
-          const SizedBox(height: 20),
-          const Text(
-            'About Us',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-            'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
-            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
-            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            style: TextStyle(
-              fontSize: 16,
-            ),
-          ),
+          
         ],
       ),
     );
